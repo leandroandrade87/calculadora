1324a3575be9939c1e356863ef8d3b6e65690522
Fernando <fernandosqa@outlook.com>
2016-12-18 15:04:42 -0200

    ajustando os steps

--- cucumber/features/steps/calcular_salario_liquido_steps.rb
+++ cucumber/features/steps/calcular_salario_liquido_steps.rb
@@ -5,24 +5,24 @@ Dado(/^que acesso a página Salário$/) do
   @salario_page.load
 end
 
-Dado(/^que eu tenho o valor do "([^"]*)"$/) do |salario|
+Dado(/^que eu tenho meu salário bruto é "([^"]*)"$/) do |salario|
   @salario_bruto = salario
 end
 
-Quando(/^eu faço o calculo de salário$/) do
+Quando(/^eu faço o calculo do meu salário$/) do
   #find('input[class$=salarioBruto]').set @salario_bruto
   @salario_page.salario_bruto.set @salario_bruto
   @salario_page.botao_calcular.click
   sleep(5)
 end
 
-Entao(/^vejo o calculo do "([^"]*)"$/) do |salario_liquido|
+Entao(/^vejo o meu salário liquido no valor de "([^"]*)"$/) do |salario_liquido|
   resultado = @salario_page.box_resultado.text
   resultado.should have_content salario_liquido
   @salario_page.tabela_resultado[5].text.should have_content salario_liquido
 end
 
-Entao(/^o valor do "([^"]*)"$/) do |imposto|
+Entao(/^o imposto de IR no valor de "([^"]*)"$/) do |imposto|
     @salario_page.tabela_resultado.each do |tr|
         tr.all('td').each do |td|
             tr.should have_content imposto if td.text == 'IRRF' 
