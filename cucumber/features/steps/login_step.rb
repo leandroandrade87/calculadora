1a2626274837070d5adce1d23927b581ad7bab19
Leandro Oliveira de Andrade <landrade@ciandt.com>
2017-11-02 14:58:58 -0200

    Teste de URL

--- cucumber/features/steps/login_step.rb
+++ cucumber/features/steps/login_step.rb
@@ -8,7 +8,7 @@ Quando(/^Preenche dados login$/) do
   #@login_page.log_in('WEBLOGIC','system32')
   fill_in 'j_username', :with => "WEBLOGIC"
   fill_in 'j_password', :with => "system32"
-  click_button 'Entrar'
+  click_button('Entrar')
 
 
 end
