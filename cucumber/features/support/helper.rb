e453dc281d0b9e383cf4351c03334e7c9ea68766
Leandro Oliveira de Andrade <landrade@ciandt.com>
2017-11-02 15:45:43 -0200

    Teste de URL

--- cucumber/features/support/helper.rb
+++ cucumber/features/support/helper.rb
@@ -11,9 +11,13 @@ module Helper
     
     # Screenshots
     def take_screenshot(file_name, result)
+      logger = Logger.new(STDOUT)
       timer_path = Time.now.strftime('%Y_%m_%d').to_s
+      logger.info("Timer_path" + timer_path)
       file_path = "reports/screenshots/test_#{result}/run_#{timer_path}"
+      logger.info("file_path" + file_path)
       screenshot = "#{file_path}/#{file_name}.png".without_accents
+      logger.info("screenshot" + screenshot)
       screenshot = screenshot.sub! ',', ''
       screenshot = screenshot.sub! '#', ''
       page.save_screenshot(screenshot)
